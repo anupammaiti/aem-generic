ad3b135e8c1761520cf2398ef0c3a4907e33a679
awadheshv <awadhesh.v@hotmail.com>
2016-05-01 12:50:36 +0530

    Changes to be committed: 	modified:   app-core/src/main/java/com/aem/app/AbstractComponent.java 	modified:   app-ui/pom.xml

--- app-core/src/main/java/com/aem/app/AbstractComponent.java
+++ app-core/src/main/java/com/aem/app/AbstractComponent.java
@@ -13,76 +13,76 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * Base class for all backing beans.
  * access to commonly used Adobe CQ APIs.
- * 
  */
 public abstract class AbstractComponent {
 
-	private final SlingHttpServletRequest request;
+    private final SlingHttpServletRequest request;
 
-	private final SlingScriptHelper slingHelper;
+    private final SlingScriptHelper slingHelper;
 
-	private final PageManager pageManager;
+    private final PageManager pageManager;
 
-	/**
-	 * Constructor for AbstractBackingBean.
-	 * @param request SlingHttpServletRequest
-	 * @param slingHelper SlingScriptHelper
-	 */
-	protected AbstractComponent(SlingHttpServletRequest request, SlingScriptHelper slingHelper) {
-		this.request = request;
-		this.slingHelper = slingHelper;
-		this.pageManager = request.getResourceResolver().adaptTo(PageManager.class);
-	}
+    /**
+     * Constructor for AbstractBackingBean.
+     *
+     * @param request     SlingHttpServletRequest
+     * @param slingHelper SlingScriptHelper
+     */
+    protected AbstractComponent(SlingHttpServletRequest request, SlingScriptHelper slingHelper) {
+        this.request = request;
+        this.slingHelper = slingHelper;
+        this.pageManager = request.getResourceResolver().adaptTo(PageManager.class);
+    }
 
-	/**
-	 * @return Current {@link HttpServletRequest}
-	 */
-	protected SlingHttpServletRequest getRequest() {
-		return request;
-	}
+    /**
+     * @return Current {@link HttpServletRequest}
+     */
+    protected SlingHttpServletRequest getRequest() {
+        return request;
+    }
 
-	/**
-	 * @return Current {@link ResourceResolver}.
-	 */
-	protected ResourceResolver getResourceResolver() {
-		return request.getResourceResolver();
-	}
+    /**
+     * @return Current {@link ResourceResolver}.
+     */
+    protected ResourceResolver getResourceResolver() {
+        return request.getResourceResolver();
+    }
 
-	/**
-	 * @return currently accessed {@link Resource}
-	 */
-	private Resource getResource() {
-		return request.getResource();
-	}
+    /**
+     * @return currently accessed {@link Resource}
+     */
+    private Resource getResource() {
+        return request.getResource();
+    }
 
-	/**
-	 * @return properties map for current JCR Node
-	 */
-	protected ValueMap getProperties() {
-		ValueMap properties = getResource().adaptTo(ValueMap.class);
-		return (properties != null) ? properties : ValueMap.EMPTY;
-	}
+    /**
+     * @return properties map for current JCR Node
+     */
+    protected ValueMap getProperties() {
+        ValueMap properties = getResource().adaptTo(ValueMap.class);
+        return (properties != null) ? properties : ValueMap.EMPTY;
+    }
 
-	/**
-	 * @return helper, providing programatic access to Sling & OSGi services
-	 * @see SlingScriptHelper#getService(Class)
-	 */
-	protected SlingScriptHelper getSlingHelper() {
-		return slingHelper;
-	}
+    /**
+     * @return helper, providing programatic access to Sling & OSGi services
+     * @see SlingScriptHelper#getService(Class)
+     */
+    protected SlingScriptHelper getSlingHelper() {
+        return slingHelper;
+    }
 
-	/**
-	 * @return {@link PageManager} implementation
-	 */
-	protected PageManager getPageManager() {
-		return pageManager;
-	}
+    /**
+     * @return {@link PageManager} implementation
+     */
+    protected PageManager getPageManager() {
+        return pageManager;
+    }
 
-	/**
-	 * @return current content {@link Page}
-	 */
-	protected Page getCurrentPage() {
-		Resource resource = getResource();
-		return (pageManager != null) ? pageManager.getContainingPage(resource) : null;
-	}
+    /**
+     * @return current content {@link Page}
+     */
+    protected Page getCurrentPage() {
+        Resource resource = getResource();
+        return (pageManager != null) ? pageManager.getContainingPage(resource) : null;
+    }
 }
