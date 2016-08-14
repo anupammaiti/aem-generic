0a4a0abb5d4a9478856436cd3abe6b067f12388d
awadheshv <awadhesh.v@hotmail.com>
2016-08-14 17:52:21 +0530

    updating scala compiler plugin

--- app-core/src/main/scala/com/aem/app/TestServletScala.scala
+++ app-core/src/main/scala/com/aem/app/TestServletScala.scala
@@ -8,12 +8,13 @@ import org.apache.sling.api.servlets.SlingSafeMethodsServlet
 import org.apache.sling.api.{SlingHttpServletRequest, SlingHttpServletResponse}
 import org.slf4j.{Logger, LoggerFactory}
 
-@SlingServlet(paths = Array("/bin/testscala"), methods = (Array("GET"))) class TestServletScala extends SlingSafeMethodsServlet {
-  private val logger: Logger = LoggerFactory.getLogger(classOf[TestServletScala])
+@SlingServlet(paths = Array("/bin/testscala"), methods = Array("GET"))
+class TestServletScala extends SlingSafeMethodsServlet {
+  val logger = LoggerFactory.getLogger(classOf[TestServletScala])
 
   @throws[ServletException]
   @throws[IOException]
-  override protected def doGet(request: SlingHttpServletRequest, response: SlingHttpServletResponse) {
+  override def doGet(request: SlingHttpServletRequest, response: SlingHttpServletResponse) = {
     logger.info("TestServletScala GET called!")
     response.getWriter.write("Hello from TestServletScala")
   }
