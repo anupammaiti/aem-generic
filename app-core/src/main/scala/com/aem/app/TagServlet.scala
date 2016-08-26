1e3963fa5dfb86e5eebe4dd6c390b546e8f0d547
awadheshv <awadhesh.v@hotmail.com>
2016-08-26 23:48:32 +0530

    Sync up

---
+++ app-core/src/main/scala/com/aem/app/TagServlet.scala
@@ -0,0 +1,21 @@
+package com.aem.app
+
+import java.io.IOException
+import javax.servlet.ServletException
+
+import org.apache.felix.scr.annotations.sling.SlingServlet
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet
+import org.apache.sling.api.{SlingHttpServletRequest, SlingHttpServletResponse}
+import org.slf4j.LoggerFactory
+
+@SlingServlet(resourceTypes = Array("generic-app/components/ctag"), methods = Array("GET"), selectors = Array("tags"), extensions = Array("json"))
+class TagServlet extends SlingSafeMethodsServlet {
+  val logger = LoggerFactory.getLogger(classOf[TagServlet])
+
+  @throws[ServletException]
+  @throws[IOException]
+  override def doGet(request: SlingHttpServletRequest, response: SlingHttpServletResponse): Unit = {
+    logger.info("TagServlet GET called!")
+    response.getWriter.write("{\"response from tagservlet\", \"OK\"}")
+  }
+}
