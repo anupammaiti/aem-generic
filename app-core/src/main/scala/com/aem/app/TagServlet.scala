460215c5a5f15bf05f2c98b59c3796751caa5c74
awadheshv <awadhesh.v@hotmail.com>
2016-08-27 09:54:42 +0530

    fixing imported-packages in bundle

--- app-core/src/main/scala/com/aem/app/TagServlet.scala
+++ app-core/src/main/scala/com/aem/app/TagServlet.scala
@@ -6,6 +6,7 @@ import javax.servlet.ServletException
 import org.apache.felix.scr.annotations.sling.SlingServlet
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet
 import org.apache.sling.api.{SlingHttpServletRequest, SlingHttpServletResponse}
+import org.apache.sling.commons.json.JSONObject
 import org.slf4j.LoggerFactory
 
 @SlingServlet(resourceTypes = Array("generic-app/components/ctag"), methods = Array("GET"), selectors = Array("tags"), extensions = Array("json"))
@@ -16,6 +17,7 @@ class TagServlet extends SlingSafeMethodsServlet {
   @throws[IOException]
   override def doGet(request: SlingHttpServletRequest, response: SlingHttpServletResponse): Unit = {
     logger.info("TagServlet GET called!")
-    response.getWriter.write("{\"response from tagservlet\", \"OK\"}")
+    //response.getWriter.write("{\"response from tagservlet1\", \"OK\"}")
+    response.getWriter.write(new JSONObject().put("response from tagservlet2", "OK").toString())
   }
 }
